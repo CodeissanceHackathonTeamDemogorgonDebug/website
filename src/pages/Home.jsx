7397256876e81ca8dd7196a9aecdf11dfa77ce8a
om-mhaske7 <omvm2005@gmail.com>
2024-10-01 02:24:02 +0530

    2.30

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,18 +21,34 @@ const Home = () => {
           <div className="card">
             <span role="img" aria-label="heart" style={styles.emoji}>❤️</span>
             <h3 style={styles.h3Card}>Compassionate Care</h3>
-            <p>Our caregivers are trained to provide empathetic and personalized care.</p>
+            <p style={styles.para}>Our caregivers are trained to provide empathetic and personalized care.</p>
           </div>
           <div className="card">
             <span role="img" aria-label="network" style={styles.emoji}>👥</span>
             <h3 style={styles.h3Card}>Trusted Network</h3>
-            <p>Access a vetted network of professional caregivers in your area.</p>
+            <p style={styles.para}>Access a vetted network of professional caregivers in your area.</p>
           </div>
           <div className="card">
             <span role="img" aria-label="reminder" style={styles.emoji}>🔔</span>
             <h3 style={styles.h3Card}>Smart Reminders</h3>
-            <p>Never miss important medications or appointments with our intelligent reminder system.</p>
+            <p style={styles.para}>Never miss important medications or appointments with our intelligent reminder system.</p>
           </div>
+          <div className="card">
+  <span role="img" aria-label="emergency-alert" style={styles.emoji}>🚨</span>
+  <h3 style={styles.h3Card}>Emergency Alert</h3>
+  <p style={styles.para}>Get instant emergency alerts and ensure prompt medical assistance when needed.</p>
+</div>
+<div className="card">
+  <span role="img" aria-label="health-data" style={styles.emoji}>📊</span>
+  <h3 style={styles.h3Card}>Health Data</h3>
+  <p style={styles.para}>Track your health metrics with real-time data and stay informed about your well-being.</p>
+</div>
+<div className="card">
+  <span role="img" aria-label="video-call" style={styles.emoji}>📞</span>
+  <h3 style={styles.h3Card}>Video Call</h3>
+  <p style={styles.para}>Connect with your doctor via secure video calls for convenient medical consultations.</p>
+</div>
+
         </div>
       </section>
     </div>
@@ -56,7 +72,7 @@ const styles = {
     fontSize: '2.9rem',
     fontWeight: 'bold',
     fontStretch: 'expanded', 
-    marginBottom: '1rem',
+    marginBottom: '2.8rem',
   },
   
   buttonGroup: {
@@ -80,11 +96,16 @@ const styles = {
   },
 
   h3Card: {
-    fontSize: '1.4rem',
+    fontSize: '1.8rem',
+    fontWeight:'560',
+  },
+
+  para: {
+    fontSize: '1.1rem',
   },
   
   emoji: {
-    fontSize: '2rem',
+    fontSize: '3rem',
   }
 };
 
